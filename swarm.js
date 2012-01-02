8620b599918e27a6dda98857225d3a405a005d41
George Lesica <glesica@gmail.com>
2012-01-01 19:09:57 -0600

    Refactored and working now.

--- swarm.js
+++ swarm.js
@@ -1,80 +1,184 @@
-BOID_RADIUS = 5;
-BOID_COUNT = 10;
+BOID_RADIUS = 3;
+SHOW_BOID_HEADING = false;
+BOID_COUNT = 100;
 MAX_SPEED = 5;
 
-SEPARATION = 0.5;
+SEPARATION = 1.0;
 SEPARATION_DISTANCE = 10;
-ALIGNMENT = 0.5;
-COHESION = 0.5;
-AVOIDANCE = 0.5;
-VISIBLE_RANGE = 50;
+ALIGNMENT = 1.0;
+COHESION = 0.2;
+AVOIDANCE = 1.0;
+VISIBILITY = 100;
+
+DEBUG = true;
+DEBUG_COUNT = 0;
 
 
-function Boid(x, y, d, m) {
-    this.x = x;
-    this.y = y;
-    this.velocity = {
-        direction: d, 
-        magnitude: m
+function debug(m) {
+    if (DEBUG === true) {
+        DEBUG = false;
+        console.debug(m);
+    }
+};
+
+
+/**
+    Vector: Represents a vector in 2D space.
+    
+    d: Vector direction, in radians
+    m: Vector magnitude or length
+*/
+function Vector(d, m) {
+    // If `d` is outside [0, 2PI), bring it back into the correct range.
+    this.direction = d % (2 * Math.PI);
+    this.magnitude = m;
+};
+// Factory that returns a vector using the given x and y coordinates.
+Vector.fromCartesian = function(pt) {
+    var dir;
+    var mag = Math.sqrt((pt.x * pt.x) + (pt.y * pt.y));
+    
+    if (pt.x >= 0) {
+        dir = ((2 * Math.PI) + Math.asin(pt.y / mag)) % (2 * Math.PI);
+    } else {
+        dir = Math.PI - Math.asin(pt.y / mag);
+    }
+    
+    // Value of `dir` should *always* be in [0, 2PI)
+    if (dir < 0 || dir >= (2 * Math.PI)) {
+        console.error('Error, angle out of range: ', dir, pt);
+    }
+
+    return new Vector(
+        dir,
+        mag
+    );
+};
+// Returns the vector represented as cartesian coordinates: {x:_, y:_}.
+Vector.prototype.asCartesian = function() {
+    return {
+        x: Math.cos(this.direction) * this.magnitude,
+        y: Math.sin(this.direction) * this.magnitude
     };
-    this.nextVelocity = {
-        direction: d, 
-        magnitude: m
-    };
-}
+};
+// Returns a copy of the vector.
+Vector.prototype.copyOf = function() {
+    return new Vector(
+        this.direction,
+        this.magnitude
+    );
+};
+// Returns the result of adding `vec`.
+Vector.prototype.addVector = function(vec) {
+    var self = this.asCartesian();
+    var other = vec.asCartesian();
+    return Vector.fromCartesian({
+        x: self.x + other.x,
+        y: self.y + other.y
+    });
+};
+// Returns the result of subtracting `vec`.
+Vector.prototype.subtractVector = function(vec) {
+    var self = this.asCartesian();
+    var other = vec.asCartesian();
+    return Vector.fromCartesian({
+        x: self.x - other.x,
+        y: self.y - other.y
+    });
+};
+// Returns the vector multiplied by a scalar.
+Vector.prototype.scaledBy = function(scl) {
+    return new Vector(
+        this.direction,
+        this.magnitude * scl
+    );
+};
+// Returns a relative vector to the given vector or point.
+Vector.prototype.vectorTo = function(other) {
+    var vec = other;
+    if (! (other instanceof Vector)) {
+        vec = Vector.fromCartesian(other);
+    }
+    return vec.subtractVector(this);
+};
+// Returns the angle to the vector or point.
+Vector.prototype.directionTo = function(other) {
+    return this.vectorTo(other).direction;
+};
+// Returns the distance to the vector or point.
+Vector.prototype.distanceTo = function(other) {
+    return this.vectorTo(other).magnitude;
+};
+
+
+/**
+    Boid: Represents a simulation unit.
+    
+    p: position vector
+    h: heading vector, should be relative to the position vector
+    i: identifier for the given boid (unused)
+*/
+function Boid(p, h, i) {
+    this.position = p.copyOf();
+    this.heading = h.copyOf();
+    this.id = i;
+    this.adjustments = [];
+};
+// Factory function that creates a random boid within the given constraints
+Boid.randomBoid = function(mx, my, mhm) {
+    var position = Vector.fromCartesian({
+        x: Math.random() * mx,
+        y: Math.random() * my
+    });
+    var heading = new Vector(
+        Math.random() * (2 * Math.PI),
+        Math.random() * mhm
+    );
+    return new Boid(position, heading);
+};
+// Draws the boid on the given graphics context.
 Boid.prototype.draw = function(ctx) {
+    var pt = this.position.asCartesian();
+    var tar = this.position.addVector(this.heading.scaledBy(5)).asCartesian();
+    // Draw the direction vector
+    if (SHOW_BOID_HEADING === true) {
+        ctx.beginPath();
+        ctx.moveTo(pt.x, pt.y);
+        ctx.lineTo(tar.x, tar.y);
+        ctx.stroke();
+    }
+
+    // Draw the dot
     ctx.beginPath();
-    ctx.arc(this.x, this.y, BOID_RADIUS, 0, Math.PI*2);
+    ctx.arc(pt.x, pt.y, BOID_RADIUS, 0, Math.PI*2);
     ctx.fill();
 }
-/**
-    Returns the distance from the current boid to `pos` 
-    where `pos` is any object with `x` and `y` properties.
-*/
-Boid.prototype.distanceTo = function(pos) {
-    return Math.sqrt(Math.pow(pos.x - this.x, 2) + Math.pow(pos.y - this.y, 2));
-}
-/**
-    Returns the average distance from the target boid 
-    to each position in `poss`. Each position can be 
-    any object with `x` and `y` properties.
-*/
-Boid.prototype.distanceToAverage = function(poss) {
-    return bs.reduce(function(prev, next) {
-        return prev + this.distanceTo(next);
-    }, null) / poss.length;
-}
-/**
-    Returns the angle from the target boid to `pos` where 
-    `pos` is any object with `x` and `y` properties.
-*/
-Boid.prototype.angleTo = function(pos) {
-    return Math.atan((pos.y - this.y) / (pos.x - this.x))
-}
-/**
-    Returns a vector from the target boid to `pos` where 
-    `pos` is any object with `x` and `y` properties.
-*/
-Boid.prototype.vectorTo = function(pos) {
-    return {
-        direction: this.angleTo(pos),
-        magnitude: this.distanceTo(pos)
+// Apply adjustments to heading then merge heading into position.
+Boid.prototype.flip = function() {
+    if (this.adjustments.length > 0) {
+        var adjVector = this.adjustments.reduce(function(prev, next) {
+            return prev.addVector(next);
+        });
+        this.adjustments = [];
+        
+        this.heading = this.heading.addVector(adjVector);
     }
-}
-/**
-    Adds the given velocity vector, `vel`, to the current 
-    "next" velocity of the target boid.
-*/
-Boid.prototype.updateVelocity = function(vel) {
-    var current = this.nextVelocity;
-    currentX = Math.cos(current.direction) * current.magnitude;
-    currentY = Math.sin(current.direction) * current.magnitude;
     
-    addX = Math.cos(vel.direction) * vel.magnitude;
-    addY = Math.cos(vel.direction) * vel.magnitude;
-    
-    
-}
+    // TODO: We mutate the heading vector here, maybe not a good idea...
+    if (this.heading.magnitude > MAX_SPEED) {
+        this.heading.magnitude = MAX_SPEED;
+    }
+    this.position = this.position.addVector(this.heading);
+};
+// Adds an adjustment vector.
+Boid.prototype.adjustHeading = function(vec) {
+    // Limit magnitude of adjustment vectors to avoid crazy course changes
+    // TODO: Shouldn't mutate vectors
+    if (vec.magnitude > MAX_SPEED) {
+        vec.magnitude = MAX_SPEED;
+    }
+    this.adjustments.push(vec.copyOf());
+};
 
 
 function Swarm(n) {
@@ -84,140 +188,130 @@ function Swarm(n) {
     
     this.boids = [];
     for (var i=0; i<n; i++) {
-        var x = Math.random() * this.width;
-        var y = Math.random() * this.height;
-        var h = Math.random() * (2 * Math.PI);
-        var s = Math.random() * MAX_SPEED;
-        this.boids.push(new Boid(x, y, h, s));
+        this.boids.push(Boid.randomBoid(this.width, this.height, MAX_SPEED));
     }
-}
-Swarm.prototype.draw = function(ctx) {
+};
+Swarm.prototype.run = function(ctx) {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    this.drawAll(ctx);
+    this.advance();
+    this.flipAll();
+};
+Swarm.prototype.drawAll = function(ctx) {
+    for (var i=0; i<this.boids.length; i++) {
+        this.boids[i].draw(ctx);
+    }
+};
+Swarm.prototype.flipAll = function() {
     this.boids.forEach(function(boid) {
-        boid.draw(ctx);
+        boid.flip();
     });
-}
-Swarm.prototype.update = function() {
+};
+Swarm.prototype.advance = function() {
     for (i in this.boids) {
         var boid = this.boids[i];
         
-        var nearest = this.boids.filter(function(b) {
-            return boid.closeTo(b, VISIBLE_RANGE);
+        var visible = this.boids.filter(function(b) {
+            return boid.position.distanceTo(b.position) <= VISIBILITY && 
+            boid !== b;
         });
         
-        /*
-            Cohesion Heading
-        */
-        var cohesionVelocity = null;
-        if (nearest.length) {
-            var sumPos = nearest.reduce(
-                function(prev, next) {
-                    return {
-                        x: prev.x + next.x,
-                        y: prev.y + next.y
-                    }
-                }
-            , {x: null, y: null});
-            var avgPos = {
-                x: sumPos.x / nearest.length,
-                y: sumPos.y / nearest.length
-            }
-            var rawVelocity = boid.vectorTo(avgPos);
-            cohesionVelocity = {
-                direction: rawVelocity.direction,
-                magnitude: rawVelocity.magnitude * COHESION
-            }
+        //debug(visible);
+        
+        // Cohesion
+        if (visible.length > 0) {
+            var cohAvgPos = visible.reduce(function(prev, next) {
+                var pt = next.position.asCartesian();
+                return {
+                    x: prev.x + (pt.x / visible.length),
+                    y: prev.y + (pt.y / visible.length)
+                };
+            }, {x: null, y: null});
+            boid.adjustHeading(
+                boid.position.vectorTo(cohAvgPos)
+                .scaledBy(COHESION)
+            );
         }
         
+        // Alignment
+        if (visible.length > 0) {
+            var alignAvgVector = visible.reduce(function(prev, next) {
+                return prev.addVector(next.heading);
+            }, new Vector(0, 0));
+            boid.adjustHeading(
+                alignAvgVector
+                .scaledBy(ALIGNMENT)
+            );
+        }
         
-        // Cohesion Heading
-        /* var dx = nearest.reduce(
-            function(prev, next) { return prev + next.x; }
-        , 0) / nearest.length - boid.x;
-        var dy = nearest.reduce(
-            function(prev, next) { return prev + next.y; }
-        , 0) / nearest.length - boid.y;
-        var dist = Math.sqrt(dx^2 + dy^2);
-        
-        var cohesionHeading = Math.acos(dx / dist); */
-        
-        // Alignment Heading
-        /* var avgH = nearest.reduce(
-            function(prev, next) { return prev + next.heading; }
-        , null) / nearest.length;
-        
-        var alignmentHeading = avgH; */
-        
-        // Separation Heading
-        // Filter the list of nearby boids to get only the ones 
-        // close enough to require separation. Find their average 
-        // position and set the separation heading to the 
-        // opposite direction (add PI).
-        /* colliders = nearest.filter(function(b) {
-            return boid.closeTo(b, SEPARATION_DISTANCE);
+        // Separation
+        // TODO: Magnitude of the effect should be inversely proportional to distance
+        var tooClose = visible.filter(function(b) {
+            return boid.position.distanceTo(b.position) <= SEPARATION_DISTANCE;
         });
         
-        var dx = colliders.reduce(
-            function(prev, next) { return prev + next.x; }
-        , 0) / colliders.length - boid.x;
-        var dy = colliders.reduce(
-            function(prev, next) { return prev + next.y; }
-        , 0) / colliders.length - boid.y;
-        var dist = Math.sqrt(dx^2 + dy^2);
-        
-        var separationHeading = Math.acos(dx / dist) + Math.PI; */
-        
-        // Avoidance Heading
-        // If the boid is near a wall, move in the opposite 
-        // direction.
-        /* var avoidanceHeading = null;
-        if (boid.x < VISIBLE_RANGE) {
-            var avoidanceHeading = 0;
-        } else if (boid.x > (this.width - VISIBLE_RANGE)) {
-            var avoidanceHeading = Math.PI;
-        } else {
-            var xHeading = null;
+        if (tooClose.length > 0) {
+            var sepAvgPos = tooClose.reduce(function(prev, next) {
+                var pt = next.position.asCartesian();
+                return {
+                    x: prev.x + (pt.x / tooClose.length),
+                    y: prev.y + (pt.y / tooClose.length)
+                };
+            }, {x: null, y: null});
+            boid.adjustHeading(
+                boid.position.vectorTo(sepAvgPos)
+                .scaledBy(-1)
+                .scaledBy(SEPARATION)
+            );
         }
         
-        if (boid.y < VISIBLE_RANGE) {
-            var yHeading = 3 * Math.PI / 2;
-        } else if (boid.y > (this.height - VISIBLE_RANGE)) {
-            var yHeading = Math.PI / 2;
-        } else {
-            var yHeading = null;
-        } */
+        // Avoidance
+        var boidPt = boid.position.asCartesian();
         
-        // Take the average direction and assign new coordinates 
-        // to the boid.
-        /* var heading = (cohesionHeading + alignmentHeading + separationHeading) / 3
-        boid.nextX = boid.x + Math.cos(heading) * 5;
-        boid.nextY = boid.y + Math.sin(heading) * 5;
-        boid.nextHeading = heading; */
+        if (boidPt.x < VISIBILITY || boidPt.x > (this.width - VISIBILITY)) {
+            boid.adjustHeading(
+                boid.position.vectorTo(Vector.fromCartesian({
+                    x: this.width / 2,
+                    y: boidPt.y
+                }))
+                .scaledBy(AVOIDANCE)
+            );
+        }
+        
+        if (boidPt.y < VISIBILITY || boidPt.y > (this.height - VISIBILITY)) {
+            boid.adjustHeading(
+                boid.position.vectorTo(Vector.fromCartesian({
+                    x: boidPt.x,
+                    y: this.height / 2
+                }))
+                .scaledBy(AVOIDANCE)
+            );
+        }
     }
-    
-    /*
-        All boids have new coordinates. Update the current 
-        positions.
-    */
-    for (i in this.boids) {
-        var boid = this.boids[i];
-        boid.x = boid.nextX;
-        boid.y = boid.nextY;
-        boid.heading = boid.nextHeading;
-        boid.speed = boid.nextSpeed;
-        
-        
-    }
-}
+};
 
 
 function run(ctx) {
     var swarm = new Swarm(BOID_COUNT);
-    swarm.draw(ctx);
-    console.log(swarm);
-    swarm.update();
-    console.log(swarm);
-    ctx.fillstyle = 'rgb(200, 50, 200)';
-    swarm.draw(ctx);
+    ctx.fillStyle = 'rgb(200, 50, 200)';
+    ctx.translate(0, canvas.height);
+    ctx.scale(1, -1);
+    
+    requestAnimationFrame = window.requestAnimationFrame || 
+                            window.mozRequestAnimationFrame ||
+                            window.webkitRequestAnimationFrame || 
+                            window.msRequestAnimationFrame;
+    var start = Date.now();
+
+    function step(timestamp) {
+        var progress = timestamp - start;
+        swarm.run(ctx);
+        if (progress < 20000) {
+            requestAnimationFrame(step);
+        }
+    }
+    requestAnimationFrame(step);
 }
+
 
 
